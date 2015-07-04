93aa87c38e75951f7fd0b4ba9306221186a2d348
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2015-07-04 01:24:00 +0100

    test: altered `test/fs-integration.test.js`
    altered to reflect changes in the past 3 commits

--- test/fs-integration.test.js
+++ test/fs-integration.test.js
@@ -1,43 +1,19 @@
-var mock = require('mock-fs');
-var assert = require('assert')
-var path = require('path')
-var os = require('os')
-var fs = require('fs')
-var fse = require('../')
-
-/* global afterEach, beforeEach, after, before, describe, it */
-
-describe('native fs', function(){
-  var TEST_DIR;
-
-  before(function(){
-    mock({});
-  });
-
-  beforeEach(function(done){
-    TEST_DIR = path.join(os.tmpdir(), 'fs-extra', 'native-fs');
-    fse.emptyDir(TEST_DIR, done);
-  });
-
-  afterEach(function(done){
-    fse.remove(TEST_DIR, done);
-  });
+describe('fs-integration:', function(){
 
   it('should use native fs methods', function(){
-    var file = path.join(TEST_DIR, 'write.txt');
-    fse.writeFileSync(file, 'hello');
-    var data = fse.readFileSync(file, 'utf8');
-    assert.equal(data, 'hello');
+    var filename = path.join(__fixtures__, 'temp.txt');
+    efe.writeFileSync(filename, 'hello');
+    assert.equal(efe.readFileSync(filename, 'utf8'), 'hello');
   });
 
   // Node.js v0.12 / IO.js
   if ( 'F_OK' in fs ) {
     it('should have native fs constants', function(){
-      assert.equal(fse.F_OK, fs.F_OK);
+      assert.equal(efe.F_OK, fs.F_OK);
+      assert.equal(efe.R_OK, fs.R_OK);
+      assert.equal(efe.W_OK, fs.W_OK);
+      assert.equal(efe.X_OK, fs.X_OK);
     });
   }
 
-  after(function(){
-    mock.restore();
-  });
 });
